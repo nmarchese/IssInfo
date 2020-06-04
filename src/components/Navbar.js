5b015c1015213ac35ba1a0fa1e5ca772c1a75737
Nicholas Marchese <nmarchese@gmail.com>
2020-06-04 20:05:03 +1000

    update navbar to use hamburger menu at lower widths

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-function Navbar() {
-  return (
-    <nav className="nav-wrapper blue darken-4">
-      <div>
-        <div className="container">
-          <Link to="/" className="brand-logo">ISS Info</Link>
-        </div>
-        <ul className="right">
-          <li><NavLink exact to="/">Home</NavLink></li>
-          <li><NavLink to="/current">Current</NavLink></li>
-          <li><NavLink to="/pass">Pass Times</NavLink></li>
-          <li><NavLink to="/individuals">Individuals in Space</NavLink></li>
-        </ul>
+class Navbar extends Component {
+
+  toggle = () => {
+    var nav = document.getElementById("nav-links");
+    if (nav.className === "right nav-list") {
+      nav.className += " dropdown";
+    } else {
+      nav.className = "right nav-list";
+    }
+  }
+
+  collapse = () => {
+    document.getElementById("nav-links").className = "right nav-list";
+  }
+
+  render() {
+    return (
+      <div className="navbar-fixed">
+        <nav className="nav-wrapper blue darken-4">
+          <div>
+            <div className="container">
+              <Link to="/" className="brand-logo" onClick={this.collapse}>ISS Info</Link>
+            </div>
+            <ul className="right nav-list" id="nav-links">
+              <li className="nav-toggle" onClick={this.toggle}><i className="material-icons">menu</i></li>
+              <li className="nav-elem"><NavLink to="/current" onClick={this.collapse}>Current</NavLink></li>
+              <li className="nav-elem"><NavLink to="/pass" onClick={this.collapse}>Pass Times</NavLink></li>
+              <li className="nav-elem"><NavLink to="/individuals" onClick={this.collapse}>Individuals in Space</NavLink></li>
+            </ul>
+          </div>
+        </nav>
       </div>
-    </nav>
-  )
+    )
+  }
 }
-// Ideas for improvement: more mobile friendly (hamburger menu at low width)
 
 export default Navbar;
